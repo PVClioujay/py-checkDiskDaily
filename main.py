610f0efdfc6c2b0ba603e598e69ec514741945cc
PVC_Jay <llioujay@gmail.com>
2017-09-16 15:20:57 +0800

    Init commit

---
+++ main.py
@@ -0,0 +1,15 @@
+###########################################################
+#                Code by PvcJay@20170822                  #
+#               It's only work on Python 2                #
+#                      Version 0.1                        #
+###########################################################
+
+import os, socket, smtplib
+from email.MIMEMultipart import MIMEMultipart
+from email.MIMEText import MIMEText
+
+if __name__ == '__main__':
+    main()
+
+def main():
+    return
